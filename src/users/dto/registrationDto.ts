f59ea4d0d4c5b7b5a94ce989fcd9589009ee851c
Sergeii Kalyna <setserg02@gmail.com>
2024-03-29 11:06:35 +0200

    rewrite dto add swagger ApiProperty

--- src/users/dto/registrationDto.ts
+++ src/users/dto/registrationDto.ts
@@ -5,18 +5,22 @@ import {
   Matches,
   IsNotEmpty,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RegistrationDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty()
   email: string;
   @IsString()
   @Length(3, 20)
   @IsNotEmpty()
+  @ApiProperty()
   firstName: string;
   @IsString()
   @Length(3, 20)
   @IsNotEmpty()
+  @ApiProperty()
   lastName: string;
   @IsString()
   @Length(0, 13)
@@ -25,6 +29,7 @@ export class RegistrationDto {
       'The numberPhone field must start with "+38" and allow an additional 10 digits.',
   })
   @IsNotEmpty()
+  @ApiProperty()
   numberPhone: string;
   @IsString()
   @Length(6, 20)
@@ -33,5 +38,6 @@ export class RegistrationDto {
       'The password field must contain 6-20 characters: only letters, digits, at least 1 special character, at least 1 uppercase letter.',
   })
   @IsNotEmpty()
+  @ApiProperty()
   password: string;
 }
