fc8c2a905769314ea8ac6b46ce7d0c79b4c77103
Sergeii Kalyna <setserg02@gmail.com>
2024-04-01 10:17:25 +0300

    add cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -17,6 +17,12 @@ export class AuthController {
   async registration(@Body() newUser: RegistrationDto, @Res() res: Response) {
     try {
       const user = await this.usersService.registration(newUser);
+      res.cookie('refreshToken', user.refreshJwt, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      });
       return res.status(200).json(user);
     } catch (error) {
       return res.status(400).json({ message: error.message });
