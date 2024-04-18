8d0dd60126fb54fe319dbcf47b4ade7b5be48c54
Sergeii Kalyna <setserg02@gmail.com>
2024-04-18 10:43:27 +0300

    fixed validation password and last name

--- src/auth/dto/registrationDto.ts
+++ src/auth/dto/registrationDto.ts
@@ -43,7 +43,7 @@ export class RegistrationDto {
 
   @IsString()
   @Length(6, 20)
-  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, {
+  @Matches(/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&().])[A-Za-z\d@$!%*?&().]{6,}$/, {
     message:
       'Password must contain 6-20 characters: letters, digits, at least 1 special character, at least 1 uppercase letter.',
   })
